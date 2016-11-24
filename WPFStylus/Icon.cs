8c43667b1beb9e423bff6ac91b7805033cafa0c6
CS472 <CS472@DESKTOP-FONJD8J>
2016-11-25 01:54:46 +0900

    add class Icon and IconSelector

---
+++ WPFStylus/Icon.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFStylus
+{
+    public class Icon
+    {
+        struct IconBound
+        {
+            public double left;
+            public double right;
+            public double top;
+            public double bottom;
+
+            public IconBound(double l, double r, double t, double b)
+            {
+                left = l; right = r; top = t; bottom = b;
+            }
+        }
+
+        private IconBound boundary;
+
+        public Icon(double b_left, double b_right, double b_top, double b_bottom)
+        {
+            boundary = new IconBound(b_left, b_right, b_top, b_bottom);
+        }
+
+        /* Eclipse Area calculate amount of 1-dimention area (in pixel)
+         * from given 2 podoubles which assume to have same x coordiante
+         * calculate the length in pixel that the line between two podoubles are
+         * in the icon box 
+         */
+        public double EclipseArea(Point above, Point below)
+        {
+            // if x-coordinate is not the same, return 0;
+            if (above.X != below.X)
+            {
+                Console.Write("EclipseArea: Error, x-coordinate is not the same\n");
+                return 0;
+            }
+
+            // out of bound
+            if (above.X < boundary.left || above.X > boundary.right)
+                return 0;
+
+            // just in case the podoubles are swap
+            double up = above.Y;
+            double down = below.Y;
+            if (up < down)
+            {
+                double temp = up;
+                up = down;
+                down = temp;
+            }
+
+            // up = MIN (up, boundary.top)
+            up = Math.Min(up, boundary.top);
+            // down = MAX (down, boundary.bottom)
+            down = Math.Max(down, boundary.bottom);
+
+            return up - down;
+        }
+
+        public double getArea()
+        {
+            return (boundary.right - boundary.left) * (boundary.top - boundary.bottom);
+        }
+    }
+}
---
+++ WPFStylus/IconSelector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;]
+using System.Windows;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFStylus
+{   
+    /* Class IconSelector
+     * ------------------
+     * Maintain list of icon and select one icon in the list
+     * such that the circle matches
+     */
+    public class IconSelector
+    {
+        List<Icon> IconArray;
+    
+        public IconSelector()
+        {
+            
+        }
+        
+        public bool AddIcon(Icon i)
+        {
+            if (!IconArray.Contains(i))
+            {
+                IconArray.Add(i);
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveIcon(Icon i)
+        {
+            
+            return false;
+        }
+
+        public Icon RemoveAt(int i)
+        {
+            return new Icon(0, 0, 0, 0);
+        }
+
+        public Point Select(ArrayList points)
+        {
+            return new Point();
+        }
+    }
+}
